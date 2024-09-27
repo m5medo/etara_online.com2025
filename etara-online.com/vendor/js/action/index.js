6980ca818defb831e6789a212c1f1a9e5c6e6e97
m5medo <134855442+m5medo@users.noreply.github.com>
2024-09-27 11:34:54 +0300

    first commit

---
+++ etara-online.com/vendor/js/action/index.js
@@ -0,0 +1,13 @@
+export default class {
+    constructor(el) {
+        this.el = el
+    }
+
+    get ref() {
+        return this.el ? this.el.ref : null
+    }
+
+    toId() {
+        return btoa(encodeURIComponent(this.el.el.outerHTML))
+    }
+}
